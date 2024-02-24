e0776b24e9e6004f8193a0c0e4c30823507860c7
pijonsauvaj <thierry2003email.fr@gmail.com>
2024-02-25 00:37:12 +0100

    j'ai changé le html, css et javascript de tous les jeux pour les rendre pareil a l'acceuil

--- script/memory3.js
+++ script/memory3.js
@@ -99,3 +99,35 @@ function startGame() {
     startTimer();
     document.querySelector('button').disabled = true; 
 }
+
+
+function toggleNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
+  
+  
+  function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      document.querySelector('.scroll-to-top').style.display = "block";
+    } else {
+      document.querySelector('.scroll-to-top').style.display = "none";
+    }
+  }
+  
+  function scrollToTop() {
+    document.body.scrollTop = 0; 
+    document.documentElement.scrollTop = 0; 
+  }
+  
+  window.onscroll = function() {
+    scrollFunction();
+  };
+  
+  
+  function redirectToProfil() {
+      window.location.href = "profil.php";
+  }
--- script/snake2.js
+++ script/snake2.js
@@ -147,3 +147,42 @@ function Fruit() {
     };
 }
 
+
+
+function toggleNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
+  
+  function search() {
+    var searchTerm = document.getElementById("searchInput").value;
+    alert("Vous avez recherché : " + searchTerm);
+  }
+  
+  function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      document.querySelector('.scroll-to-top').style.display = "block";
+    } else {
+      document.querySelector('.scroll-to-top').style.display = "none";
+    }
+  }
+  
+  function scrollToTop() {
+    document.body.scrollTop = 0; 
+    document.documentElement.scrollTop = 0; 
+  }
+  
+  window.onscroll = function() {
+    scrollFunction();
+  };
+  
+  function redirectToCart() {
+      window.location.href = "panier.php";
+  }
+  
+  function redirectToProfil() {
+      window.location.href = "profil.php";
+  }
